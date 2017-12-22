bc16d284374dde1a528b9357fd527b574fbba930
pete-dnae <pete.howard@dnae.com>
2017-12-22 22:27:28 +0000

    Finished first attempt on the set of models.

--- app/models.py
+++ app/models.py
@@ -190,6 +190,20 @@ class Plate(models.Model):
     allocation_instruction = models.ForeignKey(AlloctionInstruction, 
         related_name='plate', on_delete=models.PROTECT)
 
-class
 
-
+class Experiment(models.Model):
+    experiment_name = models.CharField(max_length=80, unique=True) 
+    designer_name = models.CharField(max_length=80) 
+    pa_mastermix = models.ForeignKey(MasterMix, 
+        related_name='experiment_pa', on_delete=models.PROTECT)
+    id_mastermix = models.ForeignKey(MasterMix, 
+        related_name='experiment_id', on_delete=models.PROTECT)
+    primer_kit = models.ForeignKey(PrimerKit, 
+        related_name='experiment_primer_kit', on_delete=models.PROTECT)
+    strain_kit = models.ForeignKey(StrainKit, 
+        related_name='experiment_strain_kit', on_delete=models.PROTECT)
+    plates = models.ManyToManyField(Plate)
+    pa_cycling = models.ForeignKey(CyclingPattern, 
+        related_name='experiment_pa_cycling', on_delete=models.PROTECT)
+    id_cycling = models.ForeignKey(CyclingPattern, 
+        related_name='experiment_id_cycling', on_delete=models.PROTECT)
