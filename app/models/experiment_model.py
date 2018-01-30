6d46fb599ab6be251d1bd58578161afbd1de9541
pete-dnae <pete.howard@dnae.com>
2018-01-30 18:16:56 +0000

    Started work on a generalised model deepcopy like utility.

--- app/models/experiment_model.py
+++ app/models/experiment_model.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from .odds_and_ends_models import CyclingPattern 
+from .odds_and_ends_models import smart_deep_copy_model 
 from .reagent_models import MasterMix
 from .primer_models import PrimerKit
 from .strain_models import StrainKit
@@ -21,7 +22,7 @@ class Experiment(models.Model):
     but it is more important today to get something to offer a promising rival
     to the status quo and to learn lessons from that.
     """
-    experiment_name = models.CharField(max_length=80, unique=True) 
+    experiment_name = models.CharField(max_length=80) 
     designer_name = models.CharField(max_length=80) 
     pa_mastermix = models.ForeignKey(MasterMix, 
         related_name='experiment_pa', on_delete=models.PROTECT)
@@ -37,27 +38,3 @@ class Experiment(models.Model):
     id_cycling = models.ForeignKey(CyclingPattern, 
         related_name='experiment_id_cycling', on_delete=models.PROTECT)
 
-    def intelligent_copy(self):
-        """
-        Knows how to make (and save) a new instance of this model, including
-        making a judgement about which attributes (recursively must also be
-        replicated, vs which can be left shared).
-        """
-        # General solution is to set primary key on self to None and then 
-        # save() self thus getting a new object with a new primary key. But 
-        # in between of course recursively copying the attributes in cases
-        # where these must not remain shared between the original and the copy.
-        self.pk = None
-
-        self.experiment_name += '_copy'
-        self.pa_mastermix = self.pa_mastermix.intelligent_copy()
-        self.id_mastermix = self.id_mastermix.intelligent_copy()
-        self.primer_kit = self.primer_kit.intelligent_copy()
-        self.strain_kit = self.strain_kit.intelligent_copy()
-
-        m2m_intelligent_copy(self.plates)
-
-        self.pa_cycling = self.pa_cycling.intelligent_copy()
-        self.id_cycling = self.id_cycling.intelligent_copy()
-
-        return self.save()
