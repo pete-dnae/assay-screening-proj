631e7a9b1db63d22db09b3afa6a4dbdd2c5754ed
pete-dnae <pete.howard@dnae.com>
2018-03-15 18:48:46 +0000

    Extending TransferRule to be able to advise about source to destination cell mappings - e.g. one-to-many and many-to-many variants.

--- app/rules_engine/transfer_rule.py
+++ app/rules_engine/transfer_rule.py
@@ -8,9 +8,23 @@ class TransferRule:
     A transfer rule specifies rows and columns on an upstream *source* plate,
     and on a downstream *destination* plate. It refers to these rows and
     columns by holding a RowColIntersection object for the source and
-    destination respectively.
+    for the destination respectively. It also holds a dilution factor for the 
+    transfer.
 
-    It also holds a dilution factor for the transfer.
+    The shape made by the rows and columns of the source does not have to match
+    exactly that made by the destination. But only some combinations are
+    allowed. Namely:
+
+    rectangle           -> exactly matching rectangle
+    horizontal strip    -> rectangle of matching width but any height (incl. 1)
+    vertical strip      -> rectangle of matching height but any width (incl. 1)
+    single cell         -> anything you like
+
+    The constructor throws IncompatibleTransferError when your combination does
+    not satisfy these constraints.
+
+    After successful construction you can access a mapping for which source
+    cells provides the source for any given destination cell in self.mapping.
     """
 
 
@@ -18,59 +32,110 @@ class TransferRule:
             conc, dilution_factor):
         """
         Provide the source and destination cells using one RowColIntersections
-        object respectively.
-
-        The class is only willing to accept some source->dest shape 
-        combinations.  The constructor will raise an IncompatibleTransferError
-        when the combination is incompatible.
+        object respectively. The constructor will raise an 
+        IncompatibleTransferError when the combination is incompatible.
         """
         self.source_plate = source_plate
         self.s_cells = source_cells
         self.d_cells = dest_cells
         self.conc = conc
         self.dilution_factor = dilution_factor
-        if not self._compatible():
-            raise IncompatibleTransferError()
+        # self.mapping works like this: using (d) for destination and (s)
+        # for source.
+        #   self.mapping[d_row][d_col] = (s_row, s_col)
+        self.mapping = {}
+        self._populate_mapping() # Can raise IncompatibleTransferError
 
 
     #-----------------------------------------------------------------------
     # Private below.
     #-----------------------------------------------------------------------
 
-    def _compatible(self):
+    def _populate_mapping(self):
         """
-        This class is only willing to accept some source->dest shape 
-        combinations. This method reports if they are compatible.
+        Works out what shape the source cells make and what shape the
+        destination cells shape makes. Then (provided they are compatible),
+        stores a mapping between them in self.mapping. Raises
+        IncompatibleTransferError if they are not compatible.
         """
 
-        s_shape, s_width, s_height = RowColIntersections(
-                self.s_cells.rows, self.s_cells.cols).shape()
-        d_shape, d_width, d_height = RowColIntersections(
-                self.d_cells.rows, self.d_cells.cols).shape()
+        s_cells = RowColIntersections(
+                self.s_cells.rows, self.s_cells.cols)
+        d_cells = RowColIntersections(
+                self.d_cells.rows, self.d_cells.cols)
+
+        s_shape, s_width, s_height = s_cells.shape()
+        d_shape, d_width, d_height = d_cells.shape()
 
         # Single source can be tranferred to any shape destination.
         if s_shape == RowColIntersections.SINGLE:
-            return True
+            self._populate_as_single_to_many(s_cells, d_cells)
+            return
 
         # Otherwise we required that BOTH the source and dest are rectangles.
         if s_shape != RowColIntersections.RECT:
-            return False
+            raise IncompatibleTransferError()
         if d_shape != RowColIntersections.RECT:
-            return False
+            raise IncompatibleTransferError()
 
         # From here on, we can depend on both source and dest being contiguous
-        # rectangles.
+        # rectangles, but this include either a width or height of one.
     
         # If the source is a single row segment, then the destination rectangle
         # must have the same width.
         if s_height == 1:
-            return d_width == s_width
+            if d_width != s_width:
+                raise IncompatibleTransferError()
+            self._populate_as_row_seg_to_rect(s_cells, d_cells)
+            return
 
         # If the source is a single column segment, then the destination 
         # rectangle must have the same height.
         if s_width == 1:
-            return d_height == s_height
+            if d_height != s_height:
+                raise IncompatibleTransferError()
+            self._populate_as_col_seg_to_rect(s_cells, d_cells)
+            return
 
         # To reach here, both the source and dest are rectangles with both 
         # dimensions > 1. In wich case their sizes must match exactly.
-        return (d_height == s_height) and (d_width == s_width)
+        if (d_height != s_height) or (d_width != s_width):
+            raise IncompatibleTransferError()
+        self._populate_as_rect_to_rect(s_cells, d_cells)
+        return
+
+    def _populate_as_single_to_many(self, s_cells, d_cells):
+        """
+        Populate self.mapping in the prior knowledge that the source cells 
+        is just a single cell, and the destination cells can be anything.
+        """
+        s_row, s_col = s_cells.all_cells()[0]
+        for d_row, d_col in d_cells.all_cells():
+            columns = self.mapping.setdefault(d_row, {})
+            columns[d_col] = (s_row, s_col)
+
+    def _populate_as_row_seg_to_rect(self, s_cells, d_cells):
+        """
+        Populate self.mapping in the prior knowledge that the source cells 
+        is a horizontal strip, and the destination cells form a rectangle of
+        the same width.
+        """
+        s_shape, s_width, s_height = s_cells.shape()
+        d_shape, d_width, d_height = d_cells.shape()
+
+        # Capture which row the source strip is, and its col range.
+        s_row, s_first_col = s_cells.all_cells()[0]
+        _, s_last_col = s_cells.all_cells()[-1]
+
+        # Populate the mapping for all the destination cells
+        for d_row, d_col in d_cells.all_cells():
+            columns = self.mapping.setdefault(d_row, {})
+            columns[d_col] = (s_row, d_col - s_first_col)
+
+
+
+
+
+
+
+
