0e6ecc1de60964dad77c7d8e96fc4a2e30d815e0
pete-dnae <pete.howard@dnae.com>
2018-03-19 11:45:01 +0000

    Added serialization of rules script interpretation to the RulesScript end point.

--- app/serializers.py
+++ app/serializers.py
@@ -1,15 +1,9 @@
 from rest_framework import serializers
 
-
 from .models.experiment_model import ExperimentModel
 from .models.rules_script_model import RulesScriptModel
+from app.rules_engine.rule_script_interpreter import RulesScriptInterpreter
 
-from app.rules_engine.rule_script_parser import ParseError
-
-
-# It would be nicer to group these class definitions in a way that aided
-# comprehension but we cannot, because they comprise nested definitions, and
-# thus must appear in dependency order for the module to load.
 
 class ExperimentSerializer(serializers.HyperlinkedModelSerializer):
 
@@ -23,15 +17,29 @@ class ExperimentSerializer(serializers.HyperlinkedModelSerializer):
 
 class RulesScriptSerializer(serializers.HyperlinkedModelSerializer):
 
-    error = serializers.SerializerMethodField(read_only=True)
+    interpretation_results = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = RulesScriptModel
         fields = (
            'url',
            'text',
-           'error',
+           'interpretation_results',
         )
 
-    def get_error(self, rule_script):
-        return ParseError('contents of parse error', 999).__dict__
+    def get_interpretation_results(self, rule_script):
+        # todo - get these from the database when they are available
+        reagents = (
+            'Titanium-Taq',
+            '(Eco)-ATCC-BAA-2355',
+            '(Eco)-ATCC-BAA-9999')
+        units = ('M/uL', 'x', 'dilution')
+
+        interpreter = RulesScriptInterpreter(
+                rule_script.text, reagents, units)
+        parse_error, alloc_table = interpreter.parse_and_interpret()
+
+        return {
+            'parse_error': None if not parse_error else parse_error.__dict__,
+            'allocation': None if not alloc_table else alloc_table.__dict__,
+        }
