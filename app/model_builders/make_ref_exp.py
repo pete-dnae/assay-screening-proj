f231917baffb0903ca6af281ebadd3dc29d44da5
pete-dnae <pete.howard@dnae.com>
2018-03-26 21:48:52 +0100

    Half way through code and end points for reagent groups. Parking for a moment to think about front end backlog.

--- app/model_builders/make_ref_exp.py
+++ app/model_builders/make_ref_exp.py
@@ -9,7 +9,7 @@ from app.models.experiment_model import ExperimentModel
 from app.models.rules_script_model import RulesScriptModel
 from app.models.reagent_model import ReagentModel
 from app.models.reagent_category_model import ReagentCategoryModel
-from app.models.reagent_group_model import ReagentCategoryModel
+from app.models.reagent_group_model import ReagentGroupModel
 from app.models.units_model import UnitsModel
 from app.model_builders.reference_data import REFERENCE_SCRIPT
 from app.model_builders.reference_data import REFERENCE_REAGENTS_DATA
@@ -26,19 +26,35 @@ class ReferenceExperiment():
         self.experiment = None
 
     def create(self):
+        # A rules script
         rules_script = RulesScriptModel.make(REFERENCE_SCRIPT)
+
+        # An experiment
         self.experiment = ExperimentModel.make(
                 'Reference Experiment', rules_script)
 
+        # Approved concentration units.
         for units in REFERENCE_UNITS:
             UnitsModel.make(units)
 
-        cats = {} # categories cache
+        # Reagent categories, and reagents
+        categories = {}
         for reagent, cat_name in REFERENCE_REAGENTS_DATA:
-            cat = cats[cat_name] if cat_name in cats else \
-                    cats.setdefault(cat_name, 
-                    ReagentCategoryModel.make(cat_name))
+            # Have to avoid creating duplicate category objects.
+            cat = categories.get(cat_name, None)
+            if cat is None:
+                cat = ReagentCategoryModel.make(cat_name)
+                categories[cat_name] = cat
             ReagentModel.make(reagent, cat)
+
+        # Make a strains groups that includes both the strain
+        # reagents we created above.
+        reagents = ReagentModel.objects.filter(category__name='Strain')
+        category = ReagentCategoryModel.objects.get(name='Strain')
+        ReagentGroupModel.make('Strain', category, reagents)
+
+
+
 
         return self.experiment
 
