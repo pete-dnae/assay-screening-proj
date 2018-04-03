3505099f6aa556e856c3b9c98d3df1bf3cfa5f3b
DNAE\Prasanna.Gnanaraj <prasanna.gnanaraj@dnae.comm>
2018-04-03 13:00:11 +0100

    css adjustments

--- webapp/src/components/hovervisualizer/hovervisualizer.vue
+++ webapp/src/components/hovervisualizer/hovervisualizer.vue
@@ -5,8 +5,8 @@ td:hover {
 </style>
 <template>
 
-<div class="w-100 ml-5 border border-light rounded bg-light " @mouseout="handleHoverLeave">
-    <div class="row text-left">
+<div class="ml-5 border border-light rounded bg-light " @mouseout="handleHoverLeave">
+    <div class=" row text-left">
             <label class="m-3 col"><strong>{{currentPlate}}</strong></label>
             <div class="col">
                 <i class="fa fa-lightbulb-o fa-2x btn" aria-hidden="true"></i>
@@ -29,9 +29,14 @@ td:hover {
                     {{String.fromCharCode(row+64)}}
                 </td>             
                 <td v-for="col in tableColCount" v-bind:key="col" class="rounded">                                                            
-                    <div id="cell" class="bg-secondary border border-secondary  rounded p-3 m-1" @mouseover="handleShowCellContents([row,col])" v-if="isItemInArray(allocationMapping[highlightedLineNumber],[col,row])&&hoverHighlight">
+                    <div  
+                        class="bg-secondary border border-secondary  rounded p-3 m-1" 
+                        @mouseover="handleShowCellContents([row,col])" 
+                        v-if="isItemInArray(allocationMapping[highlightedLineNumber],[col,row])&&hoverHighlight">
                     </div>
-                    <div id="cell" class="bg-white border border-light rounded p-3 m-1" @mouseover="handleShowCellContents([row,col])" v-else>
+                    <div  
+                        class="bg-white border border-light rounded p-3 m-1" 
+                        @mouseover="handleShowCellContents([row,col])" v-else>
                     </div>                                      
                 </td>
             </tr>
