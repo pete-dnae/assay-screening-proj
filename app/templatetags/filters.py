1095e7cffbb2b4c21819515f3a6e8c2786a4576e
DNAE\Prasanna.Gnanaraj <prasanna.gnanaraj@dnae.comm>
2018-04-20 14:15:56 +0100

    implemented nicki's feedback

--- app/templatetags/filters.py
+++ app/templatetags/filters.py
@@ -10,15 +10,19 @@ def column(dict,key):
 @register.filter
 def row(dict_key,row):
     dict,column = dict_key
-    if  column in dict and row in dict[column]:
+    if column in dict and row in dict[column]:
         string = '<ul style="padding:0;list-style-type: none">'
         for entity in dict[column][row]:
             reagent,conc,unit = entity
-            string += '<li>%s at %s %s</li><hr>' % (reagent,conc,unit)
+            string += '<li>%s %s %s</li><hr>' % (conc,unit,reagent)
         string += '</ul>'
         return string
     else:
         return ''
+
+@register.filter
+def alphabet(value):
+    return chr(value+64)
 
 register.filter('column',column)
 register.filter('row',row)
