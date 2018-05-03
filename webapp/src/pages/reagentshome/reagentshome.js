be189508ca49e4fa44e4cb4da8f9bedfd6d0d0af
DNAE\Prasanna.Gnanaraj <prasanna.gnanaraj@dnae.comm>
2018-05-03 11:39:29 +0100

    reagents list table with search functionality

--- webapp/src/pages/reagentshome/reagentshome.js
+++ webapp/src/pages/reagentshome/reagentshome.js
@@ -5,11 +5,21 @@ import _ from 'lodash';
 export default {
   name: 'reagentshome',
   data() {
-    return {};
+    return {
+      showReagents: false,
+      searchField: null,
+      searchText: null,
+    };
   },
   components: { reagentGroup },
   computed: {
-    ...mapGetters({}),
+    ...mapGetters({ reagents: 'getReagents' }),
+    filteredReagents() {
+      if (this.searchField && !_.isEmpty(this.searchText)) {
+        return this.reagents.filter(reagent => reagent[this.searchField].includes(this.searchText));
+      }
+      return this.reagents;
+    },
   },
   methods: {
     ...mapActions([]),
