ee5538c37f6db0306c32b9803569878739ddd592
DNAE\Prasanna.Gnanaraj <prasanna.gnanaraj@dnae.comm>
2018-05-09 14:55:19 +0100

    added opaque payload to reagents

--- app/models/reagent_model.py
+++ app/models/reagent_model.py
@@ -7,13 +7,16 @@ class ReagentModel(models.Model):
     """
     The reagent's name, plus the category it belongs to.
     """
+
     name = models.CharField(max_length=80, unique=True,primary_key=True)
     category = models.ForeignKey(
             ReagentCategoryModel, on_delete=models.PROTECT)
+    opaque_payload = models.CharField(max_length=200,null=True)
 
     @classmethod
-    def make(cls, name, category):
+    def make(cls, name, category,opaque_payload=None):
         return ReagentModel.objects.create(
             name = name,
-            category=category
+            category=category,
+            opaque_payload = opaque_payload
         )
