0019defc93f45f6b54c6e2c726d5069f410e454e
Chris Icely <chris.icely@dnae.com>
2018-05-10 18:00:52 +0100

    Type updates

--- clients/expt_recipes/nested/model_builders.py
+++ clients/expt_recipes/nested/model_builders.py
@@ -1,34 +1,14 @@
 from typing import Dict
 
-from clients.expt_recipes.nested.models import IdConstituents
-from clients.expt_recipes.common.models import IdQpcrData, LabChipData
+from clients.expt_recipes.common.models import IdQpcrData, LabChipData, \
+    LabChipDatas, qPCRDatas
 import clients.expt_recipes.interp.constituents as intc
-from hardware.plates import ExptPlates, WellName, Plate
+from clients.expt_recipes.nested.models import IdConstituents
+from hardware.plates import WellName
 import hardware.qpcr as hwq
 import hardware.labchip as hwlc
 
 Constituents = Dict[WellName, IdConstituents]
-qPCRDatas = Dict[WellName, IdQpcrData]
-LabChipDatas = Dict[WellName, LabChipData]
-
-
-def build_id_qpcr_constituents(
-        id_plate_reagents: Plate,
-        expt_plates: ExptPlates) -> Constituents:
-    """
-    Builds a dictionary keyed by the well names. The values are instances of
-    `IdConstituents`
-    :param id_plate_reagents: a dictionary keyed by well name and valued by
-    instances of List[ObjReagent]
-    :param expt_plates: an instance of ExptPlates for this particular
-    experiment
-    :return:
-    """
-    id_qpcr_constituents = {}
-    for w, reagents in id_plate_reagents.items():
-        id_qpcr_constituents[w] = \
-            IdConstituents.create(reagents, expt_plates)
-    return id_qpcr_constituents
 
 
 def build_id_qpcr_datas_from_inst_data(
