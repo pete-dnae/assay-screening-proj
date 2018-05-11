573f64cbb5fceb540d6f563b41d3dd8b0ad8809d
Chris Icely <chris.icely@dnae.com>
2018-05-11 12:47:46 +0100

    Removed constituents module.

--- clients/expt_recipes/common/utils.py
+++ clients/expt_recipes/common/utils.py
@@ -1,5 +1,6 @@
 from typing import Dict
 
+from clients.expt_recipes.common.models import WellConstituents
 from clients.transfers import get_parent_wells, get_dilutions
 from hardware.plates import Plate
 
@@ -39,3 +40,24 @@ def get_lc_dilutions(lc_plate: Plate) -> Dict[str, float]:
                              'qpcr well'.format(w))
         lc_dilutions[w] = dilutions[0]
     return lc_dilutions
+
+
+def is_ntc(wc: WellConstituents) -> bool:
+    """
+    Inspects a WellConstituents instances and determines whether it's an ntc.
+    :param wc: a WellConstituents instance
+    :return:
+    """
+    templates = [v for k, v in wc.items() if 'templates' in k]
+    human = [v for k, v in wc.items() if 'human' in k]
+    return not any(templates + human)
+
+
+def get_ntc_wells(wcs: Dict[str, WellConstituents])\
+        -> Dict[str, WellConstituents]:
+    """
+    Gets the ntc wells from a dictionary of WellConstituents
+    :param wcs: dictionary of WellConstituents
+    :return:
+    """
+    return dict((w, wc) for w, wc in wcs.items() if is_ntc(wc))
