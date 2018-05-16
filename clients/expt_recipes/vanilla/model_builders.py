980bf0a97d08fd9ff21a94b64d9011a296f663af
Chris Icely <chris.icely@dnae.com>
2018-05-16 18:25:46 +0100

    Decoupling and updating docs

--- clients/expt_recipes/vanilla/model_builders.py
+++ clients/expt_recipes/vanilla/model_builders.py
@@ -1,43 +1,35 @@
 
-from typing import Dict
 
-from clients.expt_recipes.common.model_builders import calc_mean_ntc_ct
-from clients.expt_recipes.common.models import IdQpcrData, qPCRDatas
-from clients.expt_recipes.vanilla.models import IdConstituents
-from clients.expt_recipes.interp.db_query import WellName
-from clients.expt_recipes.interp import qpcr as intq
-import hardware.qpcr as hwq
-
-ConstituentsByWell = Dict[WellName, IdConstituents]
+from clients.expt_recipes.common.models import IdQpcrData
+from clients.expt_recipes.common.utils import get_ntc_wells
+from clients.expt_recipes.interp.qpcr import default_ct_if_nan
+from hardware.qpcr import get_mean_ct, calc_mean_tm
 
 
-def build_id_qpcr_datas_from_inst_data(
-        id_qpcr_constituents: ConstituentsByWell,
-        instrument_data: hwq.qPCRInstPlate) -> qPCRDatas:
+def build_id_qpcr_datas_from_inst_data(id_qconsts, qinst_plate):
     """
     Creates a dictionary keyed by well names and valued by instances of
     `IdQpcrData`.
 
-    Iterates over all the wells in a plates and initially groups them by the
-    id assay and then the preamp assay. At various stages in the grouping
-    process, intermediate values are calculated as they are required when
-    creating a `IdQpcrData`.
+    Iterates over all the wells in a plate and initially groups them by the
+    id assay. At various stages in the grouping process, intermediate values
+    are calculated as they are required when creating a `IdQpcrData`.
 
-    :param id_qpcr_constituents: a dictionary keyed by well name and valued by
-    instances of `IdConstituents`
-    :param instrument_data: the python representation of qPCR results for
-    the well in question
+    :param id_qconsts: a dictionary of wells containing constituents
+    :param qinst_plate: a dictionary of wells containing instrument data
     :return:
     """
     id_qpcr_datas = {}
-    id_grouped = group_by_id_assay(id_qpcr_constituents)
+    id_grouped = _group_by_id_assay(id_qconsts)
     for id_assay, id_constits in id_grouped.items():
-        max_conc_mean_tm = \
-            calc_max_conc_mean_tm(id_constits, instrument_data)
-        mean_ntc_ct = calc_mean_ntc_ct(id_constits, instrument_data)
-        mean_ntc_ct = intq.default_ct_if_nan(mean_ntc_ct)
+        max_conc_mean_tm = _calc_max_conc_mean_tm(id_constits, qinst_plate)
+
+        ntc_wells = get_ntc_wells(id_constits)
+        qpcr_datas = [qinst_plate[w] for w in ntc_wells]
+        mean_ntc_ct = default_ct_if_nan(get_mean_ct(qpcr_datas))
+
         for w, nic in id_constits.items():
-            well_data = instrument_data[w]
+            well_data = qinst_plate[w]
             id_qpcr_datas[w] = \
                 IdQpcrData.create_from_inst_data(well_data,
                                                  max_conc_mean_tm,
@@ -45,14 +37,13 @@ def build_id_qpcr_datas_from_inst_data(
     return id_qpcr_datas
 
 
-def get_wells_by_id_assay(id_qpcr_constituents: ConstituentsByWell):
+def _get_wells_by_id_assay(id_qconsts):
     """
     Creates a dictionary keyed by id assay and valued by associated well names.
-    :param id_qpcr_constituents: a dictionary keyed by well name and valued by
-    instances of `IdConstituents`
+    :param id_qconsts: a dictionary of wells containing constituents
     :return:
     """
-    groupings = create_vanilla_groupings(id_qpcr_constituents)
+    groupings = _create_vanilla_groupings(id_qconsts)
 
     wells_by_id_assay = {}
     for id_assay in groupings:
@@ -64,41 +55,47 @@ def get_wells_by_id_assay(id_qpcr_constituents: ConstituentsByWell):
     return wells_by_id_assay
 
 
-def create_vanilla_groupings(id_qpcr_constituents):
+def _create_vanilla_groupings(id_qconsts):
+    """
+    Groups wells into a nested dictionary according to the "vanilla" grouping
+    pattern of:
+    id assay -> template
+
+    :param id_qconsts: a dictionary of wells containing constituents
+    :return:
+    """
     groups = {}
-    idgrp = group_by_id_assay(id_qpcr_constituents)
+    idgrp = _group_by_id_assay(id_qconsts)
     for id_assay, id_constits in idgrp.items():
         groups[id_assay] = {}
-        tgrp = group_by_template_origin(id_constits)
+        tgrp = _group_by_template_origin(id_constits)
         for t, t_constituents in tgrp.items():
             groups[id_assay][t] = t_constituents
     return groups
 
 
-def group_by_id_assay(constituents: ConstituentsByWell):
+def _group_by_id_assay(id_qconsts):
     """
     Groups constituents by id assay.
-    :param constituents: a dictionary keyed by well name and valued by
-    instances of `IdQpcrData`
+    :param id_qconsts: a dictionary of wells containing constituents
     :return:
     """
     wells_by_id_assay = {}
-    for w, c in constituents.items():
+    for w, c in id_qconsts.items():
         id_assay = c.get_id_assay_attribute('reagent_name')
         inner = wells_by_id_assay.setdefault(id_assay, {})
         inner[w] = c
     return wells_by_id_assay
 
 
-def group_by_template_origin(constituents: ConstituentsByWell):
+def _group_by_template_origin(id_qconsts):
     """
     Groups constituents by template origin.
-    :param constituents: a dictionary keyed by well name and valued by
-    instances of `IdQpcrData`
+    :param id_qconsts: a dictionary of wells containing constituents
     :return:
     """
     wells_by_template = {}
-    for w, c in constituents.items():
+    for w, c in id_qconsts.items():
         id_template = c.get_id_template_attribute('reagent_name')
         if id_template:
             inner = wells_by_template.setdefault('id', {})
@@ -108,41 +105,37 @@ def group_by_template_origin(constituents: ConstituentsByWell):
     return wells_by_template
 
 
-def calc_max_conc_mean_tm(constituents: ConstituentsByWell,
-                          instrument_data: hwq.qPCRInstPlate) -> float:
+def _calc_max_conc_mean_tm(id_qconsts, qinst_data):
     """
     Calculates the melting temperature ("tm") for the wells that have
     the maximum concentration of template.
 
     For nested experiments, these are exclusively the id only template wells.
 
-    :param constituents: a dictionary keyed by well name and valued by instances
-    of `IdConstituents`
-    :param instrument_data: qPCR instrument data
+    :param id_qconsts: a dictionary of wells containing constituents
+    :param qinst_data: qPCR instrument data
     :return:
     """
-    max_conc_wells = get_max_conc_template_from_id_wells(constituents)
-    qpcr_datas = [instrument_data[w] for w in max_conc_wells]
-    max_conc_mean_tm = hwq.calc_mean_tm(qpcr_datas)
+    max_conc_wells = _get_max_conc_template_from_id_wells(id_qconsts)
+    qpcr_datas = [qinst_data[w] for w in max_conc_wells]
+    max_conc_mean_tm = calc_mean_tm(qpcr_datas)
     return max_conc_mean_tm
 
 
-def get_max_conc_template_from_id_wells(
-        constituents: ConstituentsByWell) -> ConstituentsByWell:
+def _get_max_conc_template_from_id_wells(id_qconsts):
     """
     Get from the id wells, those wells that have the highest template
     concentration.
 
-    :param constituents: a dictionary keyed by well name and valued
-    by instances of `IdConstituents`
+    :param id_qconsts: a dictionary of wells containing constituents
     :return:
     """
     concs = dict((w, wc.get_id_template_attribute('concentration'))
-                 for w, wc in constituents.items())
+                 for w, wc in id_qconsts.items())
     max_conc = max(concs.values())
 
     max_conc_wells = {}
-    for w, c in constituents.items():
+    for w, c in id_qconsts.items():
         if concs[w] == max_conc:
             max_conc_wells[w] = c
     return max_conc_wells
