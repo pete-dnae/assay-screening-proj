032064ab53a4f94b5e08acd88ef2e361d4b5de67
DNAE\Prasanna.Gnanaraj <prasanna.gnanaraj@dnae.comm>
2018-07-25 18:48:13 +0100

    python version upgrade , based on herokus suggestion and also after running into some errors due to compatiblity issues

--- app/mob2dsl/nested.py
+++ app/mob2dsl/nested.py
@@ -62,20 +62,3 @@ def assert_options(options):
         if options[op] == '' or options[op] is None:
             raise ValidationError('Please provide {}'.format(op))
 
-if __name__ == '__main__':
-
-    excel_file = 'nested_input_A.xlsx'
-
-    pa_mastermix = 'pa mastermix A'
-    id_mastermix = 'id mastermix A'
-
-    pa_primer_conc = 0.4
-    pa_primers_unit = 'uM'
-    id_primer_conc = 0.9
-    id_primers_unit = 'uM'
-    dilution = 30
-
-    dsl = generate_dsl(excel_file, pa_mastermix, pa_primer_conc,
-                       pa_primers_unit, id_mastermix, id_primer_conc,
-                       id_primers_unit, dilution)
-    print('\n'.join(dsl))
